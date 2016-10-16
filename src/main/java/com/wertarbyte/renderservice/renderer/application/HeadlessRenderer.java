10369ef779a69c6c5fa0119ebefa0e554504289c
leMaik <an@maik.tk>
2016-10-17 00:58:19 +0200

    Add license and license headers.

--- src/main/java/com/wertarbyte/renderservice/renderer/application/HeadlessRenderer.java
+++ src/main/java/com/wertarbyte/renderservice/renderer/application/HeadlessRenderer.java
@@ -1,20 +1,19 @@
 /*
- * Copyright (c) 2013-2015 Wertarbyte <http://wertarbyte.com>
+ * rs-rendernode is the worker node software of our RenderService.
+ * Copyright (C) 2016 Wertarbyte <https://wertarbyte.com>
  *
- * This file is part of Wertarbyte RenderService.
- *
- * Wertarbyte RenderService is free software: you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Wertarbyte RenderService is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Wertarbyte RenderService.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.wertarbyte.renderservice.renderer.application;
