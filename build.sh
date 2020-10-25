ff4f3ee053952ca02e72aae50906be2be9337879
Maik Marschner <an@maik.tk>
2020-10-25 16:13:54 +0100

    Remove old build script.

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-docker build -t build-img -f Dockerfile.build .
-docker create --name build-cont build-img
-
-mkdir -p target
-docker cp build-cont:/usr/src/app/target ./
-docker rm build-cont
-
-docker build -t wertarbyte/rs3-rendernode .
