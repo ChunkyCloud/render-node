dc861851ea6259dc3510c1ff3f5d04e442bbfb95
Maik Marschner <an@maik.tk>
2020-12-27 20:53:19 +0100

    Update chunky.

--- src/main/java/de/lemaik/renderservice/renderer/chunky/EmbeddedChunkyWrapper.java
+++ src/main/java/de/lemaik/renderservice/renderer/chunky/EmbeddedChunkyWrapper.java
@@ -46,52 +46,48 @@ public class EmbeddedChunkyWrapper implements ChunkyWrapper {
 
     context.setRenderThreadCount(threads);
     RenderManager renderer = new RenderManager(context, true);
-    try {
-      renderer.setCPULoad(cpuLoad);
+    renderer.setCPULoad(cpuLoad);
 
-      SynchronousSceneManager sceneManager = new SynchronousSceneManager(context, renderer);
-      context.setSceneDirectory(scene.getParentFile());
-      sceneManager.getScene()
-          .loadScene(context,
-              scene.getName().substring(0, scene.getName().length() - ".json".length()),
-              new TaskTracker(ProgressListener.NONE));
+    SynchronousSceneManager sceneManager = new SynchronousSceneManager(context, renderer);
+    context.setSceneDirectory(scene.getParentFile());
+    sceneManager.getScene()
+        .loadScene(context,
+            scene.getName().substring(0, scene.getName().length() - ".json".length()),
+            new TaskTracker(ProgressListener.NONE));
 
-      renderer.setSceneProvider(sceneManager);
-      renderer.setSnapshotControl(new SnapshotControl() {
-        @Override
-        public boolean saveSnapshot(Scene scene, int nextSpp) {
-          return false;
-        }
-
-        @Override
-        public boolean saveRenderDump(Scene scene, int nextSpp) {
-          return false;
-        }
-      });
-
-      renderer.setOnRenderCompleted((time, sps) -> {
-        RenderStatus status = renderer.getRenderStatus();
-        Scene renderedScene = sceneManager.getScene();
-        renderedScene.renderTime = status.getRenderTime();
-        renderedScene.spp = status.getSpp();
-        renderedScene.saveDump(context, new TaskTracker(ProgressListener.NONE));
-        result.complete(context.getDump());
-      });
-
-      try {
-        sceneManager.getScene().setTargetSpp(targetSpp);
-        sceneManager.getScene().startHeadlessRender();
-        renderer.start();
-        renderer.join();
-        renderer.shutdown();
-      } catch (InterruptedException e) {
-        result.completeExceptionally(new RenderException("Rendering failed", e));
+    renderer.setSceneProvider(sceneManager);
+    renderer.setSnapshotControl(new SnapshotControl() {
+      @Override
+      public boolean saveSnapshot(Scene scene, int nextSpp) {
+        return false;
       }
 
-      return result;
-    } finally {
-      stopRenderer(renderer);
+      @Override
+      public boolean saveRenderDump(Scene scene, int nextSpp) {
+        return false;
+      }
+    });
+
+    renderer.setOnRenderCompleted((time, sps) -> {
+      RenderStatus status = renderer.getRenderStatus();
+      Scene renderedScene = sceneManager.getScene();
+      renderedScene.renderTime = status.getRenderTime();
+      renderedScene.spp = status.getSpp();
+      renderedScene.saveDump(context, new TaskTracker(ProgressListener.NONE));
+      result.complete(context.getDump());
+    });
+
+    try {
+      sceneManager.getScene().setTargetSpp(targetSpp);
+      sceneManager.getScene().startHeadlessRender();
+      renderer.start();
+      renderer.join();
+      renderer.shutdown();
+    } catch (InterruptedException e) {
+      result.completeExceptionally(new RenderException("Rendering failed", e));
     }
+
+    return result;
   }
 
   @Override
@@ -122,21 +118,5 @@ public class EmbeddedChunkyWrapper implements ChunkyWrapper {
   @Override
   public void setDefaultTexturepack(File texturepackPath) {
     this.defaultTexturepack = texturepackPath;
-  }
-
-  /**
-   * Stops the given renderer. The worker threads of RenderManager are created in its constructor.
-   * Without this method, they would never be removed, causing threads to leak.
-   *
-   * @param renderer Renderer to stop
-   */
-  private static void stopRenderer(RenderManager renderer) {
-    try {
-      Method stopWorkers = RenderManager.class.getDeclaredMethod("stopWorkers");
-      stopWorkers.setAccessible(true);
-      stopWorkers.invoke(renderer);
-    } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-      e.printStackTrace();
-    }
   }
 }
